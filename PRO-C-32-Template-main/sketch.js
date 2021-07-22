9284d3ccf9804a9b4794a3aef6e2aef38dc14394
tanishq6396 <83862331+tanishq6396@users.noreply.github.com>
2021-07-22 18:31:46 +0530

    Add files via upload

---
+++ PRO-C-32-Template-main/sketch.js
@@ -0,0 +1,83 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+
+var engine, world;
+var backgroundImg;
+
+var bg ;
+
+function preload() {
+    // create getBackgroundImg( ) here
+    getBackgroundImg();
+}
+
+function setup(){
+    var canvas = createCanvas(1200,700);
+    engine = Engine.create();
+    world = engine.world;
+
+}
+
+function draw(){
+
+    // add condition to check if any background image is there to add
+    if(backgroundImg)
+    background(backgroundImg)
+
+    Engine.update(engine);
+
+    // write code to display time in correct format here
+
+
+}
+
+async function getBackgroundImg(){
+
+    // write code to fetch time from API
+    var response =  await fetch("http://worldtimeapi.org/api/timezone/Asia/Tokyo")
+    //change the data in JSON format
+    var responseJSON = await response.json();
+    var datetime = responseJSON.datetime;
+    // write code slice the datetime
+    hour = datetime.slice(11,13);
+    console.log(hour)
+    // add conditions to change the background images from sunrise to sunset
+    if (hour>=06&&hour<=7){
+        bg="sprite/sunrise1.png"
+    }
+    else if (hour>=07&&hour<=8){
+        bg="sprite/sunrise2.png"
+    }
+    else if (hour>=09&&hour<=10){
+        bg="sprite/sunrise3.png"
+    }
+
+    else if (hour>=11&&hour<=12){
+        bg="sprite/sunrise4.png"
+    }
+    else if (hour>=13&&hour<=14){
+        bg="sprite/sunrise5.png"
+    }
+    if (hour>=15&&hour<=16){
+        bg="sprite/sunrise6.png"
+    }
+    if (hour>=17&&hour<=18){
+        bg="sprite/sunrise11.png"
+    }
+    if (hour>=19&&hour<=20){
+        bg="sprite/sunrise8.png"
+    }
+    if (hour>=21&&hour<=22){
+        bg="sprite/sunrise10.png"
+    }
+    if (hour>=23&&hour<=24){
+        bg="sprite/sunrise12.png"
+    }
+
+    //load the image in backgroundImg variable here
+        backgroundImg=loadImage("sprite/sunrise11.png");
+     console.log(bg);
+}
+
